e0b87647b8a4bb4d3b45f93ee1b4e1042ba65409
Daniel Wagenaar <daw@caltech.edu>
2014-11-11 10:02:05 -0500

    ,

--- src/AC_Worker.cpp
+++ src/AC_Worker.cpp
@@ -14,11 +14,44 @@ AC_Worker::AC_Worker(PhotoDB const &db, class BasicCache *cache,
 
 void AC_Worker::recache(QSet<quint64> versions) {
   addToDBQueue(versions);
-  N += versions.size();
-  // Now add to readyToLoad and activate the IF_Bank if it is partially idle.
+  markReadyToLoad(versions);
+  activateBank();
 }
 
+void AC_Worker::boot() {
+  markReadyToLoad(getSomeFromDBQueue(100));
+  activateBank();
+}
+
+QSet<quint64> AC_Worker::getSomeFromDBQueue(int maxres) {
+  QSqlQuery qq(*cache->database());
+  qq.prepare("select version from queue limit :m");
+  qq.bindValue(":m", maxres);
+  if (!qq.exec())
+    throw qq;
+  QSet<quint64> ids;
+  while (qq.next()) 
+    ids << qq.value(0).toULongLong();
+  return ids;
+}
+ 
+void AC_Worker::markReadyToLoad(QSet<quint64> versions) {
+  foreach (auto v: versions) {
+    if (beingLoaded.contains(v)) 
+      invalidatedWhileLoading << v;
+    else if (loaded.contains(v))
+      loaded.remove(v);
+    if (!rtlSet.contains(v)) {
+      readyToLoad << v;
+      rtlSet << v;
+      N++;
+    }
+  }
+}  
+
 void AC_Worker::addToDBQueue(QSet<quint64> versions) {
+  if (versions.isEmpty())
+    return;
   Transaction t(cache->database());
   QSqlQuery q(*cache->database());
   for (auto v: versions) {
@@ -35,9 +68,56 @@ void AC_Worker::addToDBQueue(QSet<quint64> versions) {
   qDebug() << "recache committed";
 }
 
+void AC_Worker::activateBank() {
+  QSet<quint64> notyetready;
+  int K = bank->availableThreads();
+  while (K>0 && !readyToLoad.isEmpty()) {
+    quint64 id = readyToLoad.dequeue();
+    if (invalidatedWhileLoading.contains(id)) {
+      notyetready << id;
+    } else {
+      rtlSet.remove(id);
+      beingLoaded.insert(id);
+      sendToBank(id);
+      K--;
+    }
+  }
+  for (auto id: notyetready) 
+    readyToLoad.push_front(id); // put back what we could not do now
+}
+    
 void AC_Worker::handleFoundImage(quint64 id, QImage img) {
   // Actually store in cache if we have enough to make it worth while
   // or if readyToLoad is empty and beingLoaded also (after removing
   // this id.)
   // Reactivate the IF_Bank if it is partially idle and we have more.
+
+  //if (urgentlyNeeded(id)) {
+    // ...
+    // emit ...
+  //}
+
+  beingLoaded.remove(id);
+
+  if (invalidatedWhileLoading.contains(id)) {
+    invalidatedWhileLoading.remove(id);
+  } else {
+    loaded[id] = img;
+  }
+
+  bool done = readyToLoad.isEmpty() && beingLoaded.isEmpty();
+  if (done || loaded.size() > threshold)
+    storeLoadedInDB();
+  emit cacheProgress(n, N);
+
+  if (done) {
+    markReadyToLoad(getSomeFromDBQueue());
+    if (readyToLoad.isEmpty()) {
+      emit doneCaching();
+      n = 0;
+      N = 0;
+    }
+  } 
+  
+  activateBank();  
 }
--- src/AC_Worker.h
+++ src/AC_Worker.h
@@ -15,14 +15,24 @@ public:
   AC_Worker(PhotoDB const &db, class BasicCache *cache,
             QObject *parent=0);
 public slots:
+  void boot(); // get initial list of cachable items from db
   void recache(QSet<quint64> versions);
   void handleFoundImage(quint64 id, QImage img);
+signals:
+  void cacheProgress(int n, int N);
+  void doneCaching();
+private:
+  QSet<quint64> getSomeFromDBQueue(int maxres);
+  void markReadyToLoad(QSet<quint64> versions);  
+  void addToDBQueue(QSet<quint64> versions);
+  void activateBank();
 private:
   PhotoDB db;
   class BasicCache *cache;
   class IF_Bank *bank;
   int n, N;
   QQueue<quint64> readyToLoad;
+  QSet<quint64> rtlSet;
   QSet<quint64> beingLoaded;
   QSet<quint64> invalidatedWhileLoading;
   QMap<quint64, QImage> loaded;
