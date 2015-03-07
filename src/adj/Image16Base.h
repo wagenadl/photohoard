db84ce3e9243519fe8823e06a28196e4b435df65
Daniel Wagenaar <wagenadl@uc.edu>
2015-03-06 21:53:36 -0500

    ,

---
+++ src/adj/Image16Base.h
@@ -0,0 +1,19 @@
+// Image16Base.h
+
+#ifndef IMAGE16BASE_H
+
+#define IMAGE16BASE_H
+
+class Image16Base {
+public:
+  enum class Format {
+    sRGB8,
+    XYZ16,
+    XYZp16,
+    Lab16,
+    LMS16,
+    IPT16,
+  };
+};
+
+#endif
---
+++ src/adj/Image16Data.h
@@ -0,0 +1,35 @@
+// Image16Data.h
+
+#ifndef IMAGE16DATA_H
+
+#define IMAGE16DATA_H
+
+#include "Image16Base.h"
+#include <QSharedPointer>
+
+class Image16Data: public QSharedData {
+public:
+  Image16Data(int w=0, int h=0,
+	      Image16Base::Format f=Image16Base::Format::sRGB8):
+    width(w), height(h), format(f),
+    image(format==Image16Base::Format::sRGB8 ? w : 3*w, h,
+	  format==Image16Base::Format::sRGB8 ? QImage::Format_RGB32
+	  : QImage::Format_RGB16), roibyteoffset(0) {
+    bytesperline = image.bytesPerLine();
+  }
+  Image16Data(QImage const &img):
+    width(img.width()), height(img.height()),
+    format(Image16Base::Format::sRGB8), image(img), roibyteoffset(0) {
+    bytesperline = image.bytesPerLine();
+  }
+public:
+  int width;
+  int height;
+  int bytesperline;
+  Image16Base::Format format;
+  QImage image;
+  int roibyteoffset;
+};
+
+
+#endif
