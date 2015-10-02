9a2bfef638ce0316e7aa08a6583a50df948e1bf8
Daniel Wagenaar <wagenadl@uc.edu>
2015-10-02 09:59:55 -0400

    The column in the undo table that holds what parameter is being changed is now called "param"; previously "key", which was not legal sql.

--- res/setupdb.sql
+++ res/setupdb.sql
@@ -110,6 +110,7 @@ create table undo (
 -- Table of undo steps
        stepid integer primary key,
        version integer,
+       param text,
        oldvalue,
        newvalue,
        undone integer default 0,
