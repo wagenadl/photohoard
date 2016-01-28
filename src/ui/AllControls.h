04184b9f0a560bcb4b3cefa5c661b97ae8cb8ecb
Daniel Wagenaar <wagenadl@uc.edu>
2016-01-27 20:39:17 -0500

    Prepping AllControls for decoupling slider names from adjustment names

--- src/ui/AllControls.h
+++ src/ui/AllControls.h
@@ -12,25 +12,24 @@ class AllControls: public QScrollArea {
 public:
   AllControls(QWidget *parent=0);
   virtual ~AllControls();
-  class ControlGroup *group(QString name) const;
-  class GentleJog *jog(QString name) const;
-  double get(QString name) const; // nan if nonexistent
-  bool contains(QString name) const { return jogs.contains(name); }
+  class ControlGroup *group(QString groupname) const;
+  class GentleJog *jog(QString slidername) const;
+  double get(QString adjustmentname) const; // nan if nonexistent
   virtual QSize sizeHint() const override;
   static class Actions const &actions();
 signals:
-  void valueChanged(QString name, double value);
+  void valueChanged(QString adjustmentname, double value);
   /* Emitted when the user changes the value and also when changed
      programmatically except for setQuietly. */
 public slots:
-  bool set(QString name, double value); // true if OK; does signal
-  bool setQuietly(QString name, double value); // true if OK; does not signal
+  bool set(QString adjustmentname, double value); // true if OK; does signal
+  bool setQuietly(QString adjustmentname, double value); // true if OK; does not signal
   void setQuietly(class Adjustments const &vv); // does not signal
 protected slots:
   void goNext(QString);
   void goPrevious(QString);
 private slots:
-  void valueChange(QString name);
+  void sliderChange(QString slidername);
 private:
   QMap<QString, ControlGroup *> groups;
   QMap<QString, GentleJog *> jogs;
