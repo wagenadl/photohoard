6dc953a927a724149df53e9366947948bd95590e
Daniel Wagenaar <wagenadl@uc.edu>
2016-02-02 19:48:04 -0800

    Minor chg to WB sliders

--- src/ui/AllControls.cpp
+++ src/ui/AllControls.cpp
@@ -103,23 +103,93 @@ GentleJog *AllControls::jog(QString name) const {
   return jogs[name];
 }
 
+/* WHITE BALANCE AND EXPOSURE
+   In a—failed—attempt to create perceptual uniformity of slider effects,
+   I coded the LMS scaling in AdjusterXYZ as
+      M ← M * 2^(ex)
+      S ← S * 2^(ex + 1/5 wb)
+      L ← L * 2^(ex + 1/15 wbg)
+   where ex is the expose parameter, wb is the whitebal parameter, and wbg
+   is the green adjust parameter.
+   What I really want is a W parameter that changes blue <-> yellow and
+   a GA parameter that changes green <-> red.
+   I assume that Intensity ~= 3G + 2R + B (approximately; linear RGB to Lch
+   maps R to L=53, G to L=87; B to L=32; there is a nonlinearity as well.)
+   Thus for DeltaI = 0, I must have 3 DeltaG + 2 DeltaR + Delta B = 0.
+   If I say that W causes DeltaB = W, then DeltaG = Delta R = W/5.
+   Likewise, if I say that GA causes DeltaG = GA, then DeltaR = 3 GA/2.
+
+   I want:
+   
+ (M)  [    ex       ]     [ E  ]
+ (S)  [ex + 1/5 wb  ] = M [ GA ]
+ (L)  [ex + 1/15 wbg]     [ W  ]
+
+ Here is how to achieve that:
+ 
+  rgb_w = [-.2 -.2 1];
+  lms_w = colorconvert(rgb_w, 'from', 'linearrgb', 'to', 'lms');
+
+  rgb_ga = [ -1 2/3 0];
+  lms_ga = colorconvert(rgb_ga, 'from', 'linearrgb', 'to', 'lms');
+
+  M = [1 1 1; lms_ga([2 3 1]); lms_w([2 3 1])]';
+  iM = inv(M);
+
+  if 1
+  fprintf(1, "static double const *wbsli2adj() {\n");
+  fprintf(1, "  static double mm[9]{\n");
+  for k=1:3
+    fprintf(1, "  %9.6f, %9.6f, %9.6f,\n",M(k,:));
+  end
+  fprintf(1, "  };\n");
+  fprintf(1, "  return mm;\n");
+  fprintf(1,"}\n\n");
+  fprintf(1, "static double const *wbadj2sli() {\n");
+  fprintf(1, "  static double mm[9]{\n");
+  for k=1:3
+    fprintf(1, "  %9.6f, %9.6f, %9.6f,\n",iM(k,:));
+  end
+  fprintf(1, "  };\n");
+  fprintf(1, "  return mm;\n");
+  fprintf(1,"}\n\n");
+  end
+ */
 static double const *wbsli2adj() {
   static double mm[9]{
-    1., .62, -.05,
-    1., -.34, .85,
-    1., .46, -.27
+   1.000000,  0.347184, -0.079930,
+   1.000000,  0.055257,  0.847499,
+   1.000000,  0.112432, -0.144067,
   };
   return mm;
 }
 
 static double const *wbadj2sli() {
   static double mm[9]{
-    -0.84234, 0.40653, 1.43581,
-     3.15315, -0.61937, -2.53378,
-     2.25225,  0.45045,  -2.70270
+  -0.436672,  0.173537,  1.263135,
+   4.193742, -0.271261, -3.922481,
+   0.241815,  0.992864, -1.234679,
   };
   return mm;
 }
+
+// static double const *wbsli2adj() {
+//   static double mm[9]{
+//     1., .62, -.05,
+//     1., -.34, .85,
+//     1., .46, -.27
+//   };
+//   return mm;
+// }
+// 
+// static double const *wbadj2sli() {
+//   static double mm[9]{
+//     -0.84234, 0.40653, 1.43581,
+//      3.15315, -0.61937, -2.53378,
+//      2.25225,  0.45045,  -2.70270
+//   };
+//   return mm;
+// }
 
 void AllControls::setAll(Adjustments const &a) {
   static QSet<QString> specials;
