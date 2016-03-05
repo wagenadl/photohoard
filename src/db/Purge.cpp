b445ca7e699ae508f7828ec126980f3f69286bf9
Daniel Wagenaar <wagenadl@uc.edu>
2016-03-04 22:13:08 -0500

    Implemented Purge

--- src/db/Purge.cpp
+++ src/db/Purge.cpp
@@ -1,21 +1,66 @@
 // Purge.cpp
 
 #include "Purge.h"
+#include "PhotoDB.h"
+#include "AutoCache.h"
 
-void purgeRejected(PhotoDB *db, AutoCache *cache) {
-  db->query("create table dupl as"
-            " select photos.id as id, count(*) as n"
-            " from versions inner join photos"
-            " on versions.photo==photos.id group by photos.id");
-  while (true) {
-    QSqlQuery q = db->constQuery("select versions.id, dupl.id"
-                                 " from versions inner join dupl"
-                                 " on versions.photo==dupl.id"
-                                 " where dupl.n>1 and versions.acceptreject<0"
-                                 " limit 1");
-    if (q.next()) {
-    } else {
-      break;
+Purge::Purge(PhotoDB *db, AutoCache *cache): db(db), cache(cache) {
+  refresh();
+}
+
+void Purge::refresh() {
+  vsnsWSiblings.clear();
+  vsnsWoSiblings.clear();
+  phsWOtherVsns.clear();
+  phsWoOtherVsns.clear();
+  phsThatCanBeDeld.clear();
+  phsThatCantBeDeld.clear();
+  
+  db->query("create table M.rejects as"
+            " select id as vsn, photo from versions"
+	    " where acceptreject<0");
+  db->query("create table M.allvsn as"
+	    " select versions.id as vsn, acceptreject, versions.photo"
+	    " from versions inner join photos on versions.photo==photos.id"
+	    " where photos.id in (select photo from M.rejects)");
+  db->query("create table M.keepphoto as"
+	    " select photo from M.allvsn where acceptreject>=0 group by photo");
+
+  {
+    QSqlQuery q = db->constQuery("select vsn, photo, photo in M.keepphoto"
+				 " from M.rejects");
+    while (q.next()) {
+      quint64_t vsn = q.value(0).toULongLong();
+      quint64_t pht = q.value(1).toULongLong();
+      bool keep = q.value(2).toBool();
+      if (keep) {
+	vsnsWSiblings.insert(vsn);
+	phsWOtherVsns.insert(pht);
+      } else {
+	vsnsWoSiblings.insert(vsn);
+	phsWoOtherVsns.insert(pht);
+      }
+    }
+  }
+
+  { QSqlQuery q = db->constQuery("select photo, filename, pathname"
+				 " from M.rejects inner join photos"
+				 " on M.rejects.photo==photos.id"
+				 " inner join folders"
+				 " on photos.folder==folders.id"
+				 " where photo not in"
+				 " (select photo from M.keepphoto)"
+				 " group by photo");
+    while (q->next()) {
+      quint64_t pht = q.value(0);
+      QFileInfo filei(q.value(2).toString() + "/" + q.value(1).toString());
+      QFileInfo folderi(q.value(2).toString());
+      bool candel = filei.isWritable() && folderi.isWritable();
+      // Note that isWritable() returns user's ability to write, not owner's.
+      if (candel)
+	phsThatCanBeDeld.insert(pht);
+      else
+	phsThatCantBeDeld.insert(pht);
     }
   }
 }
--- src/db/Purge.h
+++ src/db/Purge.h
@@ -2,8 +2,30 @@
 
 #ifndef PURGE_H
 
+#include <QSet>
+
 #define PURGE_H
 
-void purgeRejected(PhotoDB *db, AutoCache *cache);
+class Purge {
+public:
+  Purge(class PhotoDB *db, class AutoCache *cache);
+  void refresh();
+public:
+  QSet<quint64> versionsWithSiblings() const { return vsnsWSiblings; }
+  QSet<quint64> versionsWithoutSiblings() const { return vsnsWoSiblings; }
+  QSet<quint64> photosWithOtherVersions() const { return phsWOtherVsns; }
+  QSet<quint64> photosWithoutOtherVersions() const { return phsWoOtherVsns; }
+  QSet<quint64> photosThatCanBeDeleted() const { return phsThatCanBeDeld; }
+  QSet<quint64> photosThatCannotBeDeleted() const { return phsThatCantBeDeld; }
+private:
+  class PhotoDB *db;
+  class AutoCache *cache;
+  QSet<quint64> vsnsWSiblings;
+  QSet<quint64> vsnsWoSiblings;
+  QSet<quint64> phsWOtherVsns;
+  QSet<quint64> phsWoOtherVsns;
+  QSet<quint64> phsThatCanBeDeld;
+  QSet<quint64> phsThatCantBeDeld;
+};
 
 #endif
