196f2e053c857ee66d5b65ff399ac1374494aff5
Daniel A. Wagenaar <daw@caltech.edu>
2023-03-31 12:43:26 -0700

    Now OK not to have “Collection”. FilterDialog more relaxed about enabling “model” and “lens” combos.

--- src/db/Session.cpp
+++ src/db/Session.cpp
@@ -117,7 +117,7 @@ Session::Session(QString dbfn0, bool create, bool readonly, QString cacheloc):
 
     if (create) {
       for (QString root: roots) 
-        scan->addTree(QFileInfo(root).absoluteFilePath(), "Default");
+        scan->addTree(QFileInfo(root).absoluteFilePath());
     }
   }
 
