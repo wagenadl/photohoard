393cf23651c54ad2c57f54ac7637325427851161
Daniel A. Wagenaar <daw@caltech.edu>
2023-11-10 01:36:59 -0800

    Fixed bug where Filter and Tags both created readlock

--- src/db/Tags.h
+++ src/db/Tags.h
@@ -8,13 +8,13 @@
 
 class Tags {
 public:
-  /* All of these create transactions as needed */
   Tags(PhotoDB *db);
+public:
+  // these create readlocks
   int collectionRoot();
   QStringList collections();
   int findCollection(QString);
   int findOne(QString tag); // 0 for not found or -1 if not unique
-  int ensureCollection(QString); // creates if not existing
   QSet<int> findAll(QString tag); // find all tags matching name
   QSet<int> findAbbreviated(QString tag);
   int find(QString tag, int parent);
@@ -29,17 +29,20 @@ public:
   QSet<int> descendants(int tagid);
   QSet<int> children(int tagid);
   int parent(int tagid);
+  bool canUndefine(int);
+  QSet<int> applied(quint64 versionid);
+  bool couldBeNew(QString tag);
+  QString interpretation(QStringList ss);
+public: // these create transactions/write locks
+  int ensureCollection(QString); // creates if not existing
   int define(QString tag, int parent);
   int define(QString tagl); // returns 0 if not definable
   bool undefine(int); // returns true if OK, fails if applied or parent.
-  bool canUndefine(int);
   void apply(quint64 versionid, int tagid);
   void apply(QSet<quint64> const &versionid, int tagid);
   void remove(quint64 versionid, int tagid);
   void remove(QSet<quint64> const &versionid, int tagid);
-  QSet<int> applied(quint64 versionid);
-  bool couldBeNew(QString tag);
-  QString interpretation(QStringList ss);
+public:
   static QString normalCase(QString);
 private:
   QSet<int> ancestorfind(QString anc, QString dec);
