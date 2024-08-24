b6651ebafe2a08f0db3b809e75b8843ee8f340d3
Daniel A. Wagenaar <daw@caltech.edu>
2024-08-23 23:20:39 -0700

    PurgeCache now cleans rocksdb

--- src/db/PurgeCache.cpp
+++ src/db/PurgeCache.cpp
@@ -6,13 +6,17 @@
 #include <QStringList>
 #include <QDir>
 
+#include "rocksdb/db.h"
+#include "rocksdb/options.h"
+#include "rocksdb/slice.h"
+
+
 namespace PurgeCache {
   void purge(PhotoDB &db, QString cachedir) {
-    pDebug() << "purgecache";
     DBWriteLock lock(&db);
 
-    db.query(QString("attach '%1/cache.db' as C").arg(cachedir));
-    QSqlQuery q = db.constQuery("select max(dbno) from C.cache"
+    db.query("attach :a as C", cachedir + ".db");
+    QSqlQuery q = db.constQuery("select 1 from C.cache"
                            " where not C.cache.version in"
                            " (select id from versions)");
     if (!q.next()) {
@@ -20,46 +24,27 @@ namespace PurgeCache {
       db.query("detach C");
       return;
     }
-    int B = q.value(0).toInt();
-    for (int b=1; b<=B; b++) 
-      db.query(QString("attach '%1/blobs%2.db' as B%3")
-               .arg(cachedir).arg(b).arg(b));
 
-    q = db.constQuery("select version,maxdim from C.cache"
-                 " where C.cache.dbno==0"
-                 " and not C.cache.version in (select id from versions)");
+    rocksdb::Options options;
+    rocksdb::DB *rdb;
+    rocksdb::Status s = rocksdb::DB::Open(options,
+                                          std::string(cachedir.toUtf8().data()),
+                                          &rdb);
+    assert(s.ok());
+  
+    q = db.constQuery("select id from C.cache"
+                      " where not C.cache.version in"
+                      " (select id from versions)");
     while (q.next()) {
-      quint64 vsn = q.value(0).toInt();
-      int d = q.value(1).toInt();
-      QList<int> bits;
-      while (vsn>0) {
-        bits << vsn % 100;
-        vsn /= 100;
-      }
-      QString leaf(QString("%1-%2.jpg").arg(bits.takeFirst()).arg(d));
-      QStringList pathlist;
-      for (auto b: bits)
-        pathlist.push_front(QString("%1").arg(b));
-      QDir dir(cachedir + "/thumbs/" + pathlist.join("/"));
-      dir.remove(leaf);
-      while (!pathlist.isEmpty()) {
-        dir.cdUp();
-        if (!dir.rmdir(pathlist.takeLast()))
-          break;
-      }
+      quint64 id = q.value(0).toULongLong();
+      rdb->Delete(rocksdb::WriteOptions(),
+                  rocksdb::Slice((char const *)&id, sizeof(id)));
     }
+    delete rdb;
 
-    db.query("delete from C.cache where"
-             " not C.cache.version in (select id from versions)");
-
-    for (int b=1; b<=B; b++)
-      db.query(QString("delete from B%1.blobs where"
-                       " not B%2.blobs.cacheid in (select id from C.cache)")
-               .arg(b).arg(b));
-
-    for (int b=1; b<=B; b++)
-      db.query(QString("detach B%1").arg(b));
-
+    db.query("delete from C.cache"
+             " where not C.cache.version in (select id from versions)");
+    
     db.query("detach C");
     pDebug() << "purgecache done";
   }
